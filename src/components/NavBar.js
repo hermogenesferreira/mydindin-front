e1665f7cd6c03679858535d972c3f9c66937a4ef
Hermogenes Ferreira <hermogenes.ferreira@gmail.com>
2022-06-21 12:25:33 -0300

    home atualizado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
 
   return (
     <div>
-      <div className="w-screen h-[80px] z-10 text-indigo-800 fixed drop-shadlow-lg mt-8">
+      <div className="w-screen h-[80px] z-10 text-indigo-800 fixed drop-shadlow-lg mt-8 bg-white">
         <div className="px-10 flex justify-between items-center w-full h-full">
           <div className="flex items-center">
             <h1 className="text-2xl font-bold mr-4 sm:text-3xl">
@@ -35,13 +35,24 @@ const NavBar = () => {
             {!nav ? <MenuIcon className="w-8" /> : <XIcon className="w-8" />}
           </div>
         </div>
-        <ul className={!nav ? 'hidden' : 'absolute font-bold w-full px-8'}>
-          <li className="border-b-2 border-zinc-200 w-full">Home</li>
-          <li className="border-b-2 border-zinc-200 w-full">Utilização</li>
-          <li className="border-b-2 border-zinc-200 w-full">Sobre Nós</li>
-          <li className="border-b-2 border-zinc-200 w-full">Acessar</li>
+        <ul
+          className={
+            !nav ? 'hidden' : 'absolute font-bold w-full px-8 bg-white'
+          }
+        >
+          <Link to="/">
+            <li className="border-b-2 border-zinc-200 w-full">Home</li>
+          </Link>
+          <Link to="/list">
+            <li className="border-b-2 border-zinc-200 w-full">Listar</li>
+          </Link>
+          <Link to="/about">
+            <li className="border-b-2 border-zinc-200 w-full">Sobre</li>
+          </Link>
           <div className="flex flex-col my-4">
-            <button className="border-none px-8 py-3 mb-4">Acessar</button>
+            <Link to="/about">
+              <button className="border-none px-8 py-3 mb-4">Acessar</button>
+            </Link>
           </div>
         </ul>
       </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,12 @@ import imagem from '../assets/imagem.jpg';
 const Home = () => {
   return (
     <div className="w-full h-screen flex flex-col justify-between">
-      <div className="grid md:grid-cols-2 m-auto">
+      <div className="grid md:grid-cols-2 max-w-[1960px] m-auto">
         <div className="flex flex-col justify-center md:items-start w-full px-40 py-8">
-          <h1 className="text-4xl font-bold py-3">
+          <h1 className="sm:text-2xl xl:text-4xl font-bold py-3">
             Tenha controle de suas despesas!
           </h1>
-          <div className="text-2xl py-2">
+          <div className="sm:text-2xl xl:text-2xl py-2">
             <p>Você tem dificuldades para organizar suas despesas?</p>
             <p>
               Com o <strong>MyDIndin</strong>, seus problemas estão resolvidos!
@@ -18,12 +18,14 @@ const Home = () => {
             <p>informe sua receita e controle seus gastos.</p>
           </div>
           <Link to="/register">
-            <button className="py-3 px-6 my-4 font-bold">Cadastre-se</button>
+            <button className="sm:px-2 xl:py-3 xl:px-6 my-4 font-bold">
+              Cadastre-se
+            </button>
           </Link>
         </div>
         <div>
           <img
-            className="h-56 w-full object-right sm:h-72 md:h-96 lg:w-full lg:h-full"
+            className="h-56 w-full object-right md:h-96 hidden sm:block "
             src={imagem}
             alt="/"
           />
